b34eff92a4ac5cc7395c461bd898ed259e3d5f97
Samidh Talsania <samidhtalsania@gmail.com>
2015-05-02 13:37:57 +0530

    added api call for getting live matches

--- d2api.py
+++ d2api.py
@@ -5,6 +5,8 @@ import MySQLdb as mysql
 import datetime
 import dateutil.parser
 
+from dota2py import api,data
+
 app = application = bottle.Bottle()
 @app.route('/hello/<name:int>')
 def hello(name):
@@ -40,7 +42,29 @@ def match(id):
 		parent_dict["success"] = False
 	return parent_dict
 
+@app.route('/match/live')
+def match_live():
+	# parent_dict = {}
+	# match_data_list = []
+	live_games = api.get_live_league_games()
+	# parent_dict["success"] = True
+	# for games in live_games["result"]["games"]:
+	# 	child_dict = {}
+	#     try:                                    
+	#         child_dict['radiant_team'] = games["radiant_team"]["team_name"]
+	#         child_dict['dire_team'] = games["dire_team"]["team_name"]
+	#         child_dict['match_id'] = games["match_id"]
+	#         match_data_list.append(child_dict)
+	#     except KeyError:
+	#         parent_dict["success"] = False
+	# parent_dict["matches"] = match_data_list
+
+	return live_games
+
+
 if __name__ == '__main__':
     app.run(host='localhost',
         port=8000, default=True)
+
+
 
